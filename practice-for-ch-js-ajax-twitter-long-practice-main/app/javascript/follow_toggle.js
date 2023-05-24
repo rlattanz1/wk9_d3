eb6d350f05b1e5d6d937e8d326ec064a22d3a5c8
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-05-24 15:01:17 -0700

    got through follow toggle back end stuff

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -3,22 +3,39 @@ import { API, broadcast } from "./util";
 export default class FollowToggle {
   constructor(toggleButton) {
     // Your code here
+    // this.toggleButton = toggleButton
+
+    //event listener for 'click' operation
+    // this.toggleButton.addEventListener("click", (this.handleClick.bind(this)));
   }
 
-  async handleClick(event) {
+  async handleClick(e) {
+
+    // this.toggleButton === this.follow() ? this.unfollow() : this.follow();
+
+    //event listener logic for 'click'
+
+    // think about using the toggle function
+    // on the click function with a ternary calling follow and unfollow maybe???
     // Your code here
   }
 
   async follow() {
+    // this.follow() = this.unfollow()
+    //follow logic should follow a user and reassign the button to equal unfollow
     // Your code here
   }
 
   async unfollow() {
+    // this.unfollow() = this.follow()
+    //unfollow logic should unfollow a user and reassign the button to equal follow
     // Your code here
   }
 
   render() {
     switch (this.followState) {
+
+
       // Your code here
     }
   }
